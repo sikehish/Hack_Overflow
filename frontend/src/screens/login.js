6e8bb79431dbf82d5919848bdb9f8d2022295874
avinash84319 <avichinnu22@gmail.com>
2023-05-27 10:58:22 +0530

    Added login page code - avinash

---
+++ frontend/src/screens/login.js
@@ -0,0 +1,21 @@
+import TextField from '@material-ui/core/TextField';
+function Login(){
+    return <div>
+        {/* form for login page for user */}
+        <form action='/' method='post'>
+            <ul className="form-container">
+                <li>
+                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                </li>
+                <li>
+                <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+                </li>
+            </ul>
+        </form>
+        <form>
+            <Button variant="text">Forget password</Button>
+        </form>
+    </div>
+}
+
+export default Login;
