a024af84789aa3cbaa4a125d98bfc004d7907f2b
avinash84319 <avichinnu22@gmail.com>
2023-05-28 01:19:16 +0530

    footer

---
+++ frontend/src/screens/Footer.jsx
@@ -0,0 +1,37 @@
+
+function Footer(){
+    return  <section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>QUICK LINKS</h3>
+            <a href="#"> <i class="fas fa-arrow-right"></i> home </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> service </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> about </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> gallery </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> price </a>
+        </div>
+
+        <div class="box">
+            <h3>CONTACT INFO</h3>
+            <a href="#"> <i class="fas fa-phone"></i>+91 9448483336</a>
+            <a href="#"> <i class="fas fa-phone"></i>+91 9448483335</a>
+            <a href="mailto:leenacreation4u@gmail.com"> <i class="fas fa-envelope"></i>leenacreation4u@gmail.com</a>
+            <a href="mailto:avihanvaisri@gmail.com"> <i class="fas fa-envelope"></i>avihanvaisri@gmail.com</a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i>BALLARI,KARNATAKA,INDIA - 583102 </a>
+        </div>
+
+        <div class="box">
+            <h3>FOLLOW US</h3>
+            <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+            <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+            <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+        </div>
+
+    </div>
+</section>
+}
+
+export default Footer;
