a212d1a154d2683d8493cb29198016969b1eaed0
avinash84319 <avichinnu22@gmail.com>
2023-05-27 11:39:15 +0530

    Added the pages - avinash

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import {BrowserRouter as Router, Routes, Route} from'react-router-dom';
 import './App.css'
-import Login from './screens/login';
+import Login from './screens/Login';
+import Signup from './screens/Signup';
+import Forgot from './screens/Forgot';
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/login' element={<Login/>} />
           <Route path='/signup' element={<Signup/>} />
+          <Route path='/forgot' element={<Forgot/>} />
         </Routes>
       </Router>
     </>
